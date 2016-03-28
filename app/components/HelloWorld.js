1bb26ba1016723fab8953a0adff08d6cc4a54c42
tconroy <tconroy@forbes.com>
2016-03-28 12:50:55 -0400

    init commit and setup

---
+++ app/components/HelloWorld.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+
+	render: function() {
+		return (
+			<p>Hello, world? :) </p>
+		);
+	}
+
+});
+
+module.exports = HelloWorld;
